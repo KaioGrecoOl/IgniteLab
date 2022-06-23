e1837585aacab2b64d890929afb6de27f4ecfddc
Kaio G <kaiogreco91@gmail.com>
2022-06-23 19:13:58 -0300

    feat: cria componente Lesson e sidebar, alem de realizar o style da página

--- event-platform/src/main.tsx
+++ event-platform/src/main.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { client } from "./styles/lib/apollo"
+import { client } from "./lib/apollo"
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
