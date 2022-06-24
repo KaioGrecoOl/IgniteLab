dae4bb9bbe8a617e177f01f7f9fe88afe0e6af1f
Kaio G <kaiogreco91@gmail.com>
2022-06-24 15:05:31 -0300

    feat: cria funcionalidades de rotas e adiciona mais style no componente lessons

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,11 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
-
 
 function App() {
   return (
-      <Event />
-  )
-    
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
