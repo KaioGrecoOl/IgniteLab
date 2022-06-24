258ec4a72011731998a439b1aa478d7e5c788269
Kaio G <kaiogreco91@gmail.com>
2022-06-24 15:46:45 -0300

    feat: adiciona as informações pelo graphql

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -42,13 +42,19 @@ interface VideoProps {
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
     variables: {
       slug: props.lessonSlug
     }
   })
 
-  console.log(data);
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
   
 
 
@@ -57,7 +63,7 @@ export function Video(props: VideoProps) {
       <div className="bg-black flex-justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="SO4-izct7Mc" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -67,27 +73,23 @@ export function Video(props: VideoProps) {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
+            {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Nessa aula vamos dar início ao projeto criando a estrutura base da
-              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
-              realizar o setup do nosso projeto no GraphCMS criando as entidades
-              da aplicação e integrando a API GraphQL gerada pela plataforma no
-              nosso front-end utilizando Apollo Client.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 at-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/KaioGrecoOl/"
+                src={data.lesson.teacher.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Kaio Greco</strong>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
                 <span className="text-gray-200 text-sm block">
-                  Estudante de desenvolvimento
+                {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
