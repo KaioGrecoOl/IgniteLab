ab3f3c025dce54b195036f3a5763174db92dc903
Kaio G <kaiogreco91@gmail.com>
2022-10-13 15:33:28 -0300

    build: :construction: cria primeira interface de tamanho de texto e colocar o size padão como md, caso não seja passado um

--- react-design-system/react-design-system/src/components/Text.tsx
+++ react-design-system/react-design-system/src/components/Text.tsx
@@ -1,4 +1,8 @@
-export function Text() {
+interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+}
+
+export function Text({ size = 'md' }: TextProps) {
   return (
     <span className="text-gray-100 font-sans">Hello World</span>
   )
