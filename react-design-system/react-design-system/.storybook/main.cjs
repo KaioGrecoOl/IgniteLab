36696795983a1a8a8ed74b5232f526a63f701dfe
Kaio G <kaiogreco91@gmail.com>
2022-10-14 16:45:15 -0300

    fix: test

--- react-design-system/react-design-system/.storybook/main.cjs
+++ react-design-system/react-design-system/.storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base === '/IgniteLab/react-design-system/'
+      config.base === '/IgniteLab/'
     }
     return config;
   }
